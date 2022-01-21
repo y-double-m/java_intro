294d9af65ec5398b0192f9e6d16c278abe9b757c
y-double-m <75093446+y-double-m@users.noreply.github.com>
2022-01-22 04:28:24 +0900

    Add files via upload

---
+++ Data_and_operation/Letter.java
@@ -0,0 +1,13 @@
+
+public class Letter {
+
+	public static void main(String[] args) {
+		
+		String name = "YANG";
+		System.out.println("Hello, "+name+" ... bye");
+
+		double VAT = 10.0;
+		System.out.println(VAT);
+	}
+
+}
